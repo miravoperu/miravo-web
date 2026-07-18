eead4ef984498156f65004297c001a7a32d7e1fa
miravoperu <miravoperu@gmail.com>
2026-07-18 15:45:01 -0500

    fix: corregir import de Footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import portfolio from "./components/Portfolio/portfolio";
 import about from "./components/About/about";
 import testimonials from "./components/Testimonials/testimonials";
 import contact from "./components/Contact/contact";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
